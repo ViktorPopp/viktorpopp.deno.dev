c9dff495d5cb1b53a7497215793d698d85a5e775
Viktor Popp <viktorpopp.dev@gmail.com>
2025-04-27 18:53:35 +0200

    retry

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,5 @@
 // @ts-check
-import { defineConfig } from "astro/config";
-import deno from "@deno/astro-adapter";
+import { defineConfig } from 'astro/config';
 
-export default defineConfig({
-    output: "server",
-    adapter: deno(),
-});
+// https://astro.build/config
+export default defineConfig({});
